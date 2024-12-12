33dc4b7ef2cea667d653b044117b5f3bd0e387c7
Ingrid Vianna <ingrid.vianna1@outlook.com>
2024-12-12 18:09:04 -0300

    :construction: config score and cardsprites

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,8 +1,14 @@
 const state ={
     score: {
         playerScore: 0,
-        ComputerScore: 0
-    }
+        ComputerScore: 0,
+        scoreBox: document.getElementById("score_points"),
+    },
+    cardSprites: {
+        avatar: document.getElementById("card-image"),
+        name: document.getElementById("card-name"),
+        type: document.getElementById("card-type"),
+    },
 }
 
 function init () {
