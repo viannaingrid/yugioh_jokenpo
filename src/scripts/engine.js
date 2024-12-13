b91cbe5b5ed063e4d3456e2acfb75c1a0c3f4243
Ingrid Vianna <ingrid.vianna1@outlook.com>
2024-12-13 15:46:11 -0300

    :contruction: config drawcards

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -18,6 +18,11 @@ const state ={
     }
 };
 
+const playersSides = {
+    player1: "player-field-card",
+    player2: "computer-field-card",
+}
+
 const pathImages = ".src/assets/icons/";
 
 const cardData = [
@@ -47,8 +52,18 @@ const cardData = [
     }
 ];
 
-function init () {
+async function drawCards(cardNumbers, fieldSide) {
+    for(let i = 0; i < cardNumbers; i++) {
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
 
+        document.getElementById(fieldSide).appendChild(cardImage);
+    }
+}
+
+function init () {
+    drawCards(5, playersSides.player1);
+    drawCards(5, playersSides.player2);
 }
 
 init();
